5257a7f617e01e0d57c37799e18322a41e815a14
r7kamura <r7kamura@gmail.com>
2020-11-06 02:50:02 +0900

    Add basic tests for Rack::Capture.call

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in rack-capture.gemspec
 gemspec
 
+gem 'fakefs'
 gem 'rake', '~> 12.0'
 gem 'rspec', '~> 3.0'
 gem 'rubocop'
--- spec/rack/capture_spec.rb
+++ spec/rack/capture_spec.rb
@@ -1,11 +1,53 @@
 # frozen_string_literal: true
 
-RSpec.describe Rack::Capture do
-  it 'has a version number' do
-    expect(Rack::Capture::VERSION).not_to be nil
-  end
+require 'pp' # Must be called before fakefs.
+require 'fakefs/spec_helpers'
+require 'json'
+require 'pathname'
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+RSpec.describe Rack::Capture do
+  describe '.call' do
+    include FakeFS::SpecHelpers
+
+    subject do
+      described_class.call(
+        app: app,
+        url: url
+      )
+    end
+
+    let(:app) do
+      lambda { |_env|
+        [
+          200,
+          { 'Content-Type' => 'text/html' },
+          %w[]
+        ]
+      }
+    end
+
+    context 'with GET /index.html' do
+      let(:url) do
+        'http://example.com/'
+      end
+
+      it 'creates dist/index.html' do
+        subject
+        pathname = Pathname.new('dist/index.html')
+        expect(pathname).to exist
+      end
+    end
+
+    context 'with GET /' do
+      let(:url) do
+        'http://example.com/'
+      end
+
+      it 'creates dist/index.html' do
+        subject
+        pathname = Pathname.new('dist/index.html')
+        expect(pathname).to exist
+      end
+    end
   end
 end
